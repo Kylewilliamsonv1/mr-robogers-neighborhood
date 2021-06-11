eb8860d7ae4e4f1c8549bbf871842b999a57a3a0
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-11 11:30:07 -0700

    confirm inputBeep function works

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-function inputBeep(number, element)
-  let element = []
-  let number = $(#submission).val()
-  for (let i = 0; i < number.length; i ++) {
-    if (number === 1) {
-      return "Beep!"
-    } else if (number === 2) {
-      return "Boop!"
-    } else if (number === 3) {
-      return "Won't you be me neighbor!"
+$(document).ready(funciton() {
+  $("formOne").submit(function(event) {
+    const numberArray = [1, 2, 3];
+    //const number = 2;
+    let number=$("#submission").val()
+    console.log(number)
+    function inputBeep(number, numberArray) {
+      for (let i = 0; i < numberArray.length; i ++) {
+        if (number === 1) {
+          return "Beep!";
+        } else if (number === 2) {
+          return "Boop!"
+        } else if (number === 3) {
+          return "Won't you be me neighbor!";
+        }
+      }
     }
   }
+}
+inputBeep(number, numberArray);
