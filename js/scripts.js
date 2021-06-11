315202f06c4b9acc84d9b10b28d7260b62eeca7f
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-11 13:05:17 -0700

    WIP merge functions to work simultaneously

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
   $("#game").submit(function(event) {
     event.preventDefault();
     const numberArray = [1, 2, 3];
-    //const number = 2;
-    let number=$("#submission").val()
-    console.log(number)
+    let inputNumber=$("#submission").val()
+    let number = []
+    number.push(inputNumber.split(""))
+    const inputNumberArray = number
     function inputBeep(number, numberArray) {
       for (let i = 0; i < numberArray.length; i ++) {
+        number.push(numberArray[i])
         if (number === 1) {
           return "Beep!";
+          console.log(number);
         } else if (number === 2) {
           return "Boop!"
         } else if (number === 3) {
