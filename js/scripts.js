53519ddd4492d402e6fc92d1d1c4d94b85613fa2
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-11 09:58:32 -0700

    add forloop and update variable names

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-function inputBeep(#submission)
+function inputBeep(number, element)
+  let element = []
   let number = $(#submission).val()
   for (let i = 0; i < number.length; i ++) {
     if (number === 1) {
       return "Beep!"
+    } else if (number === 2) {
+      return "Boop!"
+    } else if (number === 3) {
+      return "Won't you be me neighbor!"
     }
   }
