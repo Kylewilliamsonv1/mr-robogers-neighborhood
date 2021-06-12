15cba1c8f510f8965bfc2b0190566c614b383c9f
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-11 18:47:05 -0700

    refine tests & logic'

--- REAME.md
+++ REAME.md
@@ -1,26 +1,20 @@
-Describe: inputBeep()
-Numbers that contain a 1; replace all digits with "Beep!"
+Describe: inputNumber()
+Objective: Numbers that contain a 1; replace all digits with "Beep!"
 
-Test:
+Test: If inputNumber is equal to 1 return "Beep!"
+Code: 
+inputNumber===1;return "Beep!"
+Expected Output: 1
+
+Objective: Numbers that contain a 2; replace all digits with "Boop!"
+
+Test: If inputNumber is equal to 2 return "Boop!"
 Code:
-const number = 1;
-inputBeep(1);
-Expected Output: "Beep!"
+inputNumber===2;return "Boop!"
+Expected Output: 1
 
-Describe: inputBoop()
-Numbers that contain a 2; replace all digits with "Boop!"
-
-Test:
+Objective: Numbers that contain a 3; replace all digits with "Won't you be me neighbor!"
+Test: If inputNumber is equal to 3 return "Won't you be me neighbor!"
 Code:
-const number = 2;
-inputBeep(2);
-Expected Output: "Boop!"
-
-Describe: inputNeighbor()
-Numbers that contain a 3; replace all digits with "Won't you be me neighbor!"
-
-Test:
-Code:
-const number = 3;
-inputBeep(3);
-Expected Output: "Won't you be me neighbor!"
+inputNumber===3;return "Won't you be me neighbor!"
+Expected Output: 1
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
   $("#game").submit(function(event) {
     event.preventDefault();
-    const numberArray = [1, 2, 3];
     let inputNumber=$("#submission").val()
-    let number = []
-    number.push(inputNumber.split(""))
-    const inputNumberArray = number
-    function inputBeep(number, numberArray) {
-      for (let i = 0; i < numberArray.length; i ++) {
-        number.push(numberArray[i])
-        if (number === 1) {
-          return "Beep!";
-          console.log(number);
-        } else if (number === 2) {
-          return "Boop!"
-        } else if (number === 3) {
-          return "Won't you be me neighbor!";
-        }
-      }
-    };
+    if (inputNumber === 1) {
+      return "Beep!"
+    } else if (inputNumber === 2) {
+      return "Boop!"
+    } else if (inputNumber === 3) {
+      return "Won't you be me neighbor!";
+    }
+    console.log(inputNumber);
+
+
+    //const conditionsArray = [1, 2, 3];
+    let numberArray = [];
+    inputNumberArray.forEach(function(element) {
+      numberArray.concat(element *1);
+    });
+    numberArray;
   });
 });
+
+
+
+
+
+
+    // const inputNumberArray = number
+    // function inputBeep(number, numberArray) {
+    //   for (let i = 0; i < numberArray.length; i ++) {
+    //     number.push(numberArray[i])
+        
+    //   }
