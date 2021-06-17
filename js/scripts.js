e8801835947b45ab0827e43a41d42d7b8045e652
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-17 15:10:58 -0700

    for loop works, WIP incorporate remaining business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,40 @@
-$(document).ready(function() {
-  $("#game").submit(function(event) {
-    event.preventDefault();
-    let userInput = 5
-    const numberArray = [1,2,3]
-    function arrayBuilder(userInput, numberArray) {
-      for (let i = 0; i < numberArray.length; i ++) {
-        console.log(numberArray);
-        if (i.tostring().includes("3")) {
-          i = "Won't you be me neighbor!";
-          numberArray.push(i); 
-        } else if (i.tostring().includes("2")) {
-          i = "Boop!";
-          numberArray.push(i); 
-        } else if (i.tostring().includes("1")) {
-          i = "Beep!";
-          numberArray.push(i);
-        } else {
-          numberArray.push(i);
-        }
-      };
-    };
-    arrayBuilder(userInput, numberArray);
-  });
-});
+// This works
+let input = 4
+let numberArray = []
+for (let i = 0; i < input; i ++) {
+  if (i.toString().includes("3")) {
+    i = "Won't you be me neighbor!";
+  }
+  numberArray.push(i)
+};
+
+// let input = 5
+// let numberArray = []
+// for (let i = 0; i < input; i ++) {
+//   if (i.toString().includes("3")) {
+//     i = "Won't you be me neighbor!";
+//   } else if (i.toString().includes("2")) {
+//     i = "Boop!"; 
+//   } else if (i.toString().includes("1")) {
+//     i = "Beep!";
+//   } else {
+//   }
+//   numberArray.push(i)
+// };
+
+  // } else if (i.toString().includes("2")) {
+  //   i = "Boop!"; 
+  // } else if (i.tostring().includes("1")) {
+  //   i = "Beep!";
+  // } else {
+
+
+
+
+
+
+
+      //user interface Logic______________________________________
+// $(document).ready(function() {
+//   $("#game").submit(function(event) {
+//     event.preventDefault();
