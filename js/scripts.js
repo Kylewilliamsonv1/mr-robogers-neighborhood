2451066d1c612e0dc9ab261aa9c8300a306e4be7
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-17 18:01:27 -0700

    branch statement works,testing example inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
 // This works
-let input = 4
-let numberArray = []
-for (let i = 0; i < input; i ++) {
-  if (i.toString().includes("3")) {
-    i = "Won't you be me neighbor!";
-  }
-  numberArray.push(i)
-};
+let input = "16"
+if (input.toString().includes(3)) {
+  input= "Won't you be me neighbor!"
+} else if (input.toString().includes(2) && !input.toString().includes(3)) {
+  input= "Boop!";
+} else if (input.toString().includes(1) && !input.toString().includes(2) && !input.toString().includes(3)) {
+  input= "Beep!"
+} else {
+  input
+}
 
-// let input = 5
+
+
+
 // let numberArray = []
-// for (let i = 0; i < input; i ++) {
-//   if (i.toString().includes("3")) {
+// for (let i = 0; i <= input; i ++) {
+//   if (i === 3) {
 //     i = "Won't you be me neighbor!";
-//   } else if (i.toString().includes("2")) {
-//     i = "Boop!"; 
-//   } else if (i.toString().includes("1")) {
-//     i = "Beep!";
-//   } else {
 //   }
 //   numberArray.push(i)
 // };
+
+
+  // if (i.toString().includes("3")) {
+  //   i = "Won't you be me neighbor!";
+  //   numberArray.push(i)
+  // } else if (i === "2") {
+  //   i = "Boop!";
+  //   numberArray.push(i) 
+  // } else if (i === "1") {
+  //   i = "Beep!";
+  //   numberArray.push(i)
+  // } else {
+  // }
+
+
+
 
   // } else if (i.toString().includes("2")) {
   //   i = "Boop!"; 
