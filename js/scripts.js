6d514d3433c5219ae8a22f7413fc317f82e0a3a8
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-17 23:04:50 -0700

    add UI logic to tie in

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,28 @@
-let input = 5
-let numberArray = []
-for (let i = 0; i <= input; i ++) {
-  if (i.toString().includes(3)) {
-    numberArray.push("Won't you be me neighbor!");
-  } else if (i.toString().includes(2)) {
-    numberArray.push("Boop!");
-  } else if (i.toString().includes(1)) {
-    numberArray.push("Beep!");
-  } else {
-    numberArray.push(i);
-  }
-
-};
-
-// let input = 5
-// let numberArray = []
-// for (let i = 0; i <= input; i ++) {
-//   if (input.toString().includes(3)) {
-//     i= "Won't you be me neighbor!"
-//   } else if (input.toString().includes(2) && !input.toString().includes(3)) {
-//     i= "Boop!";
-//   } else if (input.toString().includes(1) && !input.toString().includes(2) && !input.toString().includes(3)) {
-//     i= "Beep!"
-//   } else {
-//     i
-//   }
-//   numberArray.push(i)
-// };
+//Business Logic________________________
+function roboLanguage (input) {
+  let numberArray = [];
+  for (let i = 0; i <= input; i ++) {
+    if (i.toString().includes(3)) {
+      numberArray.push("Won't you be me neighbor!");
+    } else if (i.toString().includes(2)) {
+      numberArray.push("Boop!");
+    } else if (i.toString().includes(1)) {
+      numberArray.push("Beep!");
+    } else {
+      numberArray.push(i);
+    }
+  };
+  return numberArray;
+}
 
 
-
-
-
-//User Interface Logic
-// $(document).ready(function() {
-//   $("#game").submit(function(event) {
-//     event.preventDefault();
-//     let input = $("#numberInput").val();
-//     if (input.toString().includes(3)) {
-//       input= "Won't you be me neighbor!"
-//     } else if (input.toString().includes(2) && !input.toString().includes(3)) {
-//       input= "Boop!";
-//     } else if (input.toString().includes(1) && !input.toString().includes(2) && !input.toString().includes(3)) {
-//       input= "Beep!"
-//     } else {
-//       input
-//     }
-//   });
-// });
+//User Interface Logic_________________________
+$(document).ready(function() {
+  $("#game").submit(function(event) {
+    event.preventDefault();
+    const input = $("#numberInput").val();
+    roboLanguage(input);
+    console.log(input)
+  
+  });
+});  
